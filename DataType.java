dfab3c15f5e503385eece8ae8bc49e91190d705c
Rohit patil <124617849+Rohit03112002@users.noreply.github.com>
2024-03-18 15:58:44 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,22 @@
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+			
+			
+			int value1 =9 / 2; //integer division 
+			float value2= 101/61f;
+			
+			double value3= 10d/ 6d;
+			
+			float value4=5/2.5f;   //float division 
+			
+			System.out.println(value1);
+			System.out.println(value2);
+			System.out.println(value3);
+			System.out.println(value4);
+
+	}
+
+}
